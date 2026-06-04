c60322d63697b2beace5977c35ab14087753b566
ravin11charli <ravindra.yadav@costacloud.com>
2026-06-04 16:50:52 +0530

    Add collectCoverageFrom configuration to Jest
    add teh collectCoverageFrom in jest.config.cjs

--- jest.config.cjs
+++ jest.config.cjs
@@ -19,4 +19,9 @@ module.exports = {
   testMatch: ['<rootDir>/src/__tests__/**/*.{test,spec}.{ts,tsx}'],
   testPathIgnorePatterns: ['/node_modules/'],
   coverageDirectory: 'coverage',
+  collectCoverageFrom: [       
+    'src/**/*.{ts,tsx}',
+    '!src/main.tsx',     
+    '!src/vite-env.d.ts',
+  ],
 }
